d4e5107613f5901d2aa877e927777f604d3faf1d
mattehardman <mattehardman@gmail.com>
2015-12-12 09:35:56 +0000

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+      templateUrl: 'partials/login.html', 
+      controller: 'LoginController'});
+  $routeProvider.when('/dashboard', {
+      templateUrl: 'partials/dashboard.html', 
+      controller: 'DashController'});
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/* Controllers */
+
+var myApp = angular.module('myApp.controllers', []);
+myApp.controller('LoginController', ['$scope','$location', function($scope,$location) {
+      $scope.creds={username:"",password:""};
+      $scope.login = function(){
+            if($scope.creds.username == 'admin' && $scope.creds.password =='1234'){
+            $location.path('/dashboard');
+        } else {
+            $('.alert-box').show();
+        }
+      };
+      $scope.logout = function(){
+          $location.path('/');
+      }
+  }]);
+
+  myApp.controller('DashController', ['$scope', function($scope) {
+      $scope.title="Dashboard";
+  }]);
+
+  myApp.controller('CustomerController', ['$scope', function($scope) {
+      $scope.customers = [
+          {name: 'John Doe', address: '155 Main St', city: 'Boston MA'},
+          {name: 'Steve Smith', address: '233 Mass Ave', city: 'Cambridge MA'},
+          {name: 'Carol Stevens', address: '12 Congress St', city: 'Amesbury MA'},
+          {name: 'Kyle Savon', address: '111 School St', city: 'Amesbury MA'},
+          {name: 'Benjamin Button', address: '122 Union St', city: 'Lynn MA'},
+          {name: 'Derek Salmon', address: '15 Marston St', city: 'Lawrence MA'}
+      ];
+  }]);
